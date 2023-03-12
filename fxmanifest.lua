a64097fdddcba02f4275b194d4f70bce5bbcc71e
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2023-03-11 23:08:49 -0500

    initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version  "cerulean"
+use_experimental_fxv2_oal   "yes"
+lua54       "yes"
+game        "gta5"
+
+name        "x-vehicleremote"
+version     "0.0.0"
+repository  "https://github.com/XProject/x-vehicleremote"
+description "Project-X Vehicle Remote: Synced Vehicle Remote & Engine System"
+
+shared_scripts {
+    "shared/*.lua",
+}
+
+server_scripts {
+    "server/*.lua"
+}
+
+client_scripts {
+    "client/*.lua",
+}
