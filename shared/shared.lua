bb3d91bfe9dbb0dea0704efc5ee031b04ff55c2d
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2023-03-12 06:24:38 -0400

    feat: experimental syncing of the vehicle engine

--- shared/shared.lua
+++ shared/shared.lua
@@ -1,1 +1,44 @@
 Shared = {}
+
+Shared.currentResourceName = GetCurrentResourceName()
+
+Shared.State = {}
+
+Shared.State.globalStartedEngines = ("%s_globalStartedEngines"):format(Shared.currentResourceName)
+
+Shared.State.vehicleEngine = ("%s_vehicleEngine"):format(Shared.currentResourceName)
+
+---@alias plate string
+
+---@class startedEngines
+---@field [plate] boolean
+
+function dumpTable(table, nb)
+    if nb == nil then
+        nb = 0
+    end
+
+    if type(table) == 'table' then
+        local s = ''
+        for i = 1, nb + 1, 1 do
+            s = s .. "    "
+        end
+
+        s = '{\n'
+        for k, v in pairs(table) do
+            if type(k) ~= 'number' then k = '"' .. k .. '"' end
+            for i = 1, nb, 1 do
+                s = s .. "    "
+            end
+            s = s .. '[' .. k .. '] = ' .. dumpTable(v, nb + 1) .. ',\n'
+        end
+
+        for i = 1, nb, 1 do
+            s = s .. "    "
+        end
+
+        return s .. '}'
+    else
+        return tostring(table)
+    end
+end
